62990be943ced1fb6482dd39b2e844cef90a2238
caleb jephuneh <calebjephunneh@gmail.com>
2023-01-12 11:53:45 -0800

    coode

---
+++ pages/succes.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+function succes() {
+  return (
+    <div ClassName="success">
+        <h1>
+          Success you have loggen in to the app
+        </h1>
+    </div>
+  )
+}
+
+export default succes
