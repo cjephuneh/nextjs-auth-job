00901e426d8adfd622c3702e24645be53a4ea196
caleb jephuneh <calebjephunneh@gmail.com>
2023-01-13 07:47:48 -0800

    coode

---
+++ pages/Login.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className='root12'>
+      <div className='rectangle1__log'></div>
+      <div className='donTHaveAnAccount'>Don’t Have An Account?</div>
+      <div className='getStartedByCreatingYourNewAcc'>Get Started By Creating Your New Account</div>
+      <div className='undraw_Mobile_login_re_9ntv1'></div>
+      <div className='rectangle2__log'></div>
+      <div className='rectangle5__log'></div>
+      <div className='rectangle3__log'></div>
+      <div className='rectangle6__log'></div>
+      <div className='rectangle7__log'></div>
+      <div className='rectangle8__log'></div>
+      <div className='rectangle4__log'></div>
+      <div className='register'>Register</div>
+      <div className='welcomeBack'>Welcome Back !</div>
+      <div className='or'>Or</div>
+      <div className='logIn'>Log -in</div>
+      <div className='weAreHappyToHaveYouBack'>We Are Happy To Have You Back</div>
+      <div className='forgotPassword'>Forgot password ?</div>
+      <div className='emailOrPhoneNumber'>Email or Phone number</div>
+      <div className='password'>Password</div>
+      <div className='hidden1'></div>
+      <div className='line1'></div>
+      <div className='line2'></div>
+      <div className='icons8Google481'></div>
+      <div className='icons8AppleLogo301'></div>
+      <div className='icons8FacebookF321'></div>
+    </div>
+  )
+}
+
+export default Login
--- pages/Register.js
+++ pages/Register.js
@@ -1,35 +1,42 @@
 import Image from 'next/image'
-import registerpic from '../public/undraw_Access_account_re_8spm.png'
-import facebook from '../public/icons8-facebook-f-32.png'
-import apple from '../public/icons8-apple-logo-30.png'
-import google from '../public/icons8-google-48.png'
+
 
 const Register = () => {
   return (
-    <div className='v1_2'>
-        <div className='create__rec'>
-            <Image className='reg__image'
-            src={registerpic}
-            alt="Picture of the author"
-            width={500}
-            height={500}    
-        />
-        <p className='v4_37'>Already Having An  Account ?</p>
-        <p className='v4_38'>We are happy to have you back</p>
-        <div className="v4_40">Log-in</div>
-        
-
+    <div>
+      <div className='undraw_Access_account_re_8spm1'></div>
+      <div className='rectangle2__reg'></div>
+      <div className='rectangle7__reg'></div>
+      <div className='rectangle1__reg'></div>
+      <div className='rectangle8__reg'></div>
+      <div className='rectangle6__reg'></div>
+      <div className='rectangle3__reg'></div>
+      <div className='rectangle4__reg'></div>
+      <div className='rectangle5__reg'></div>
+      <div className='createAccount__reg'>Create Account</div>
+      <div className='alreadyHavingAnAccount__reg'>Already Having An Account ?</div>
+      <div className='weAreHappyToHaveYouBack__reg'>We are happy to have you back</div>
+      <div className='register__reg'>Register</div>
+      <div className='logIn__reg'>Log-in</div>
+      <div className='getStartedByCreatingYourOwnAcc__reg'>Get started by creating your own account</div>
+      <div className='byClickingYouAgreeToTheTermsAn__reg'>
+        <div className='textBlock__reg'>By clicking you agree to the terms and</div>
+        <div className='textBlock2__reg'> conditions</div>
       </div>
-
-      <div className='left__reg'>
-        <p className='create__acc'> Create Account</p>
-        <p className='get_st'> Get started by creating your own account</p>
-        <div className='overlap-group1'>
-          <span className='full-name inter-normal-black-20px'>full name</span>
-        </div>
-      </div>
-      
-       
+      <div className='fullName__reg'>Full Name</div>
+      <div className='emailAddress__reg'>Email Address</div>
+      <div className='phoneNumber__reg'>Phone Number </div>
+      <div className='password__reg'>Password</div>
+      <div className='hidden1__reg'></div>
+      <div className='or__reg'>Or</div>
+      <div className='line1__reg'></div>
+      <div className='rectangle9__reg'></div>
+      <div className='rectangle10__reg'></div>
+      <div className='rectangle11__reg'></div>
+      <div className='icons8Google481__reg'></div>
+      <div className='icons8AppleLogo30__reg'></div>
+      <div className='icons8FacebookF32__reg'></div>
+      <div className='line2__reg'></div>
     </div>
   )
 }
