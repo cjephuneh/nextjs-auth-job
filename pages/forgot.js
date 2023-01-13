33f7cd614e707eedd2232b404ecabd8009597beb
caleb jephuneh <calebjephunneh@gmail.com>
2023-01-13 03:30:50 -0800

    coode

---
+++ pages/forgot.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+const forgot = () => {
+  return (
+    <div className='root'>
+      <div className='rectangle1'></div>
+      <div className='rectangle2'></div>
+      <div className='rectangle3'></div>
+      <div className='undraw_Forgot_password_re_hxwm'></div>
+      <div className='forgotYourPassword'>Forgot Your Password? </div>
+      <div className='enterYourEmailOrPhoneNumber'>Enter Your Email Or Phone Number</div>
+      <div className='andSetANewPassword'>And Set A New Password</div>
+      <div className='sendCode'>Send Code</div>
+      <div className='enterEmailOrPhoneNumber'>Enter Email or Phone number</div>
+    </div>
+  )
+}
+
+export default forgot
