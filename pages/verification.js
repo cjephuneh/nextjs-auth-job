d9e08f926586b589f6a08a3db03c1ba35a97847f
caleb jephuneh <calebjephunneh@gmail.com>
2023-01-13 04:34:26 -0800

    cooode

---
+++ pages/verification.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const verification = () => {
+  return (
+    <div>
+         <div className='rectangle1_ver'></div>
+      <div className='verificationCode'>Verification Code</div>
+      <div className='didnTRecieveAnOtpResend'>
+        <p className='labelWrapper'>
+          <span className='label'>Didn’t recieve an Otp ? </span>
+          <span className='label2'>Resend</span>
+        </p>
+      </div>
+      <div className='_254708419386'>+254708419386</div>
+      <div className='weHaveSentYouAVerificationCode'>
+        <div className='textBlock'>We have sent you a verification code </div>
+        <div className='textBlock2'> to your mobile number</div>
+      </div>
+      <div className='rectangle2__ver'></div>
+      <div className='rectangle3__ver'></div>
+      <div className='rectangle4__ver'></div>
+      <div className='rectangle6__ver'></div>
+      <div className='rectangle5__ver'></div>
+      <div className='submitContinue'>Submit &amp; Continue</div>
+      <div className='icons8Edit241'></div>
+      <div className='undraw_Confirm_re_69me1'></div>
+    </div>
+  )
+}
+
+export default verification
